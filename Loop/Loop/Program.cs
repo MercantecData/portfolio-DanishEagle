13f976fbc8503b1d3c5d0b04b8d73cf7985e21cd
Emilie <emilie.jakobsen2@gmail.com>
2019-11-05 12:16:42 +0100

    Exercise 3 from Opsættning af Portfolio

--- Loop/Loop/Program.cs
+++ Loop/Loop/Program.cs
@@ -6,7 +6,21 @@ namespace Loop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Array(4);
+        }
+
+        static int[] Array(int num1)
+        {
+            int[] yes = new int[num1];
+            int i = 0;
+
+            while (i < num1)
+            {
+                yes[i] = i + 1;
+                Console.WriteLine(yes[i]);
+                i++;
+            }
+            return yes;
         }
     }
 }
